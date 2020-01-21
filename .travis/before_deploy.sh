9f6d581151d3d78b5a57e6f0941021a5fb5e53ad
Dana Van Aken <dvanaken@cs.cmu.edu>
2020-01-21 10:01:00 -0500

    checkpoint

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -7,3 +7,5 @@ for tag in base web driver
 do
     docker tag "ottertune-${tag}" "${DOCKER_REPO}:${tag}" > /dev/null 2>&1
 done
+
+echo "$DOCKER_PASSWD" | docker login -u "$DOCKER_USER" --password-stdin > /dev/null 2>&1
